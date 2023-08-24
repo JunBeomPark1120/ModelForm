2a52daf8d62d3b411b89566599fbf61e97c9bc50
junbeompark <a01077348062@gmail.com>
2023-08-24 14:17:27 +0900

    Delete 기능 구현

--- articles/views.py
+++ articles/views.py
@@ -64,3 +64,10 @@ def create(request):
     # 4. create.html을 랜더링
     # 9. crate. html을 랜더링
     return render(request, 'create.html', context)
+
+def delete(request, id):
+    article = Article.objects.get(id=id)
+    
+    article.delete()
+    
+    return redirect('articles:index')
